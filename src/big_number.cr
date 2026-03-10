c1ad1956baf6406adec7111e2b74b511a423ad26
Jack Thorne <jack@kilmer.co>
2026-03-10 12:58:09 -0500

    Add type extensions for Int, Float, String interop
    Enables mixed arithmetic (42 + BigInt), Int#to_big_i,
    String#to_big_i(base), Float#to_big_i, and BigInt.new(Float).

--- src/big_number.cr
+++ src/big_number.cr
@@ -1,5 +1,6 @@
 require "./big_number/limb"
 require "./big_number/big_int"
+require "./big_number/ext"
 
 module BigNumber
   VERSION = "0.1.0"
---
+++ src/big_number/ext.cr
@@ -0,0 +1,97 @@
+struct Int
+  def +(other : BigNumber::BigInt) : BigNumber::BigInt
+    BigNumber::BigInt.new(self) + other
+  end
+
+  def &+(other : BigNumber::BigInt) : BigNumber::BigInt
+    self + other
+  end
+
+  def -(other : BigNumber::BigInt) : BigNumber::BigInt
+    BigNumber::BigInt.new(self) - other
+  end
+
+  def &-(other : BigNumber::BigInt) : BigNumber::BigInt
+    self - other
+  end
+
+  def *(other : BigNumber::BigInt) : BigNumber::BigInt
+    BigNumber::BigInt.new(self) * other
+  end
+
+  def &*(other : BigNumber::BigInt) : BigNumber::BigInt
+    self * other
+  end
+
+  def %(other : BigNumber::BigInt) : BigNumber::BigInt
+    BigNumber::BigInt.new(self) % other
+  end
+
+  def <=>(other : BigNumber::BigInt) : Int32
+    -(other <=> self)
+  end
+
+  def ==(other : BigNumber::BigInt) : Bool
+    other == self
+  end
+
+  def gcd(other : BigNumber::BigInt) : Int
+    BigNumber::BigInt.new(self).gcd(other).to_i64
+  end
+
+  def lcm(other : BigNumber::BigInt) : BigNumber::BigInt
+    BigNumber::BigInt.new(self).lcm(other)
+  end
+
+  def to_big_i : BigNumber::BigInt
+    BigNumber::BigInt.new(self)
+  end
+end
+
+struct Float
+  def <=>(other : BigNumber::BigInt) : Int32?
+    return nil if nan?
+    BigNumber::BigInt.new(self) <=> other
+  end
+
+  def to_big_i : BigNumber::BigInt
+    BigNumber::BigInt.new(self)
+  end
+end
+
+class String
+  def to_big_i(base : Int32 = 10) : BigNumber::BigInt
+    BigNumber::BigInt.new(self, base)
+  end
+end
+
+module BigNumber
+  struct BigInt
+    def initialize(num : Float::Primitive)
+      @limbs = Pointer(Limb).null
+      @alloc = 0
+      @size = 0
+      raise ArgumentError.new("Non-finite float") unless num.finite?
+      return if num == 0
+      neg = num < 0
+      # Truncate toward zero
+      mag = neg ? (-num).to_u128 : num.to_u128
+      set_from_unsigned(mag)
+      @size = -@size if neg
+    end
+
+    def initialize(other : BigInt)
+      if other.zero?
+        @limbs = Pointer(Limb).null
+        @alloc = 0
+        @size = 0
+      else
+        n = other.abs_size
+        @alloc = n
+        @limbs = Pointer(Limb).malloc(n)
+        @limbs.copy_from(other.@limbs, n)
+        @size = other.@size
+      end
+    end
+  end
+end
